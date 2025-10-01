5cd52a376e6f12b499f0b904b97c862061bbf5b0
seonyoungg <tjsduddlfma1@naver.com>
2025-10-01 21:56:48 +0900

    💄 style: 기본애니메이션 컴포넌트 크기 통일

--- src/components/basic/Skew.tsx
+++ src/components/basic/Skew.tsx
@@ -8,7 +8,7 @@ export default function SkewBox() {
       initial={{ skewX: 30, skewY: 10 }} // 초기 비스듬한 상태
       animate={{ skewX: 0, skewY: 0 }} // 원래 형태로 복원
       transition={{ duration: 1, ease: "easeOut" }}
-      className="w-32 h-32 bg-yellow-500 rounded-md flex items-center justify-center text-white font-bold"
+      className="w-24 h-24 bg-yellow-500 rounded-md flex items-center justify-center text-white font-bold"
     >
       Skew
     </motion.div>
